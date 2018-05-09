580f1325ac5343616121cb998c87fd22686daa6e
Clayton-ty <c.t.waldock@outlook.com>
2018-05-09 15:09:18 +1000

    git cloudinary working local host

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 gem 'stripe'
 gem 'devise'
+# gem "paperclip", "~> 6.0.0"
 gem 'bootstrap-sass', '~> 3.3.7'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,16 +1,16 @@
 class Listing < ApplicationRecord
+
+  # if Rails.env.development?
+  #   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.svg.png"
+  # else
+  #   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.svg.png",
+  #                     :storage => :dropbox,
+  #                     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+  #                     :path => ":style/:id_:filename"
+  # end
+  #
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   mount_uploader :image, ListingImageUploader
-
-  if Rails.env.development?
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.svg.png"
-  else
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.svg.png",
-                      :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                      :path => ":style/:id_:filename"
-  end
-
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
